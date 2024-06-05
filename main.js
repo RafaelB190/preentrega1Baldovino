b50235c9fa0d55cd190a6892818b94b0d9b86a4c
 Emma <tumailrafaeleb36@gmail.com>
2024-06-04 21:53:08 -0300

    correcciones

--- main.js
+++ main.js
@@ -1,47 +1,51 @@
 let producto = " ";
-let cantidad = 0 ;
+let cantidad = 0;
 let cantidadTotal = 0;
-let precio = 0 ;
+let precio = 0;
 let precioTotal = 0;
 let seguirComprando = false;
 
-const validarCantidad = (cantidad) => {
-    while (Number.isNaN(cantidad) || cantidad === 0) {
-     if (cantidad !== 0) {
-        alert("agregar cantidad")
-     } else {
-        alert("agregar numero distinto de cero")
-     }
-     cantidad = parseInt(prompt ("¿Cuántos querés comprar?"));}
-     return cantidad;
-    };    
-
 do {
-    producto = prompt("comprar sorrentinos, humita o ambosPromo ");
-    cantidad = parseInt( prompt("ingrese cantidad desaeada de compra"));
+    producto = prompt("Comprar sorrentinos, humita o ambosPromo ");
+    cantidad = parseInt(prompt("Ingrese cantidad deseada de compra"));
     
+    const validarCantidad = (cantidad) => {
+        while (Number.isNaN(cantidad) || cantidad === 0) {
+            if (cantidad !== 0) {
+                alert("Agregar cantidad");
+            } else {
+                alert("Agregar número distinto de cero");
+            }
+            cantidad = parseInt(prompt("¿Cuántos querés comprar?"));
+        }
+        return cantidad;
+    };
+
     const cantidadValidada = validarCantidad(cantidad);
-    ;
-    switch (producto) {
+    
+    switch (producto.toLowerCase()) { // Agregar .toLowerCase() para hacer la entrada no sensible a mayúsculas
         case "sorrentinos": 
             precio = 3000;
             break;
         case "humita":
             precio = 1500;
             break;
-        case "ambosPromo":
+        case "ambospromo":
             precio = 4250;
             break;
         default: 
             alert("Datos incorrectos");
-            precio= 0;
-            cantidad= 0;
-    }        
-    precioTotoal += precio * cantidadValidada;
-    cantidadTotal += cantidad;   
+            precio = 0;
+            cantidad = 0;
+    }
+    
+    precioTotal += precio * cantidadValidada;
+    cantidadTotal += cantidadValidada;
 
-    seguirComprando = confirm("Quiere algo mas?");
-}while (seguirComprando);
+    seguirComprando = confirm("¿Quiere algo más?");
+} while (seguirComprando);
 
-alert("Adquisicion de {cantidadTotal} productos y el total es ${precioTotal}")
+alert(`Adquisición de ${cantidadTotal} productos y el total es $${precioTotal}`);
+alert("muchas gracias por su compra");
+
 
